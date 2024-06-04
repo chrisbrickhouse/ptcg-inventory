a126b8736a017a5827c8c2d3383f34d40b85fc40
Christian Brickhouse <brickhouse@stanford.edu>
2024-06-03 22:18:29 -0700

    [WIP] api tests

--- cardsite/api/tests.py
+++ cardsite/api/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.humanize.templatetags import humanize
+from django.test import RequestFactory, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .api import get_n_card_in, index
+from cardstash.models import CardStash
+
+class ApiInterfaceTests( TestCase ):
+    fixtures = [
+            'sv3',
+        ]
+
+    def setUp( self ):
+        self.request_factory = RequestFactory()
+        self.logged_in_user = User.objects.create_user(
+                username = "api_test",
+                email = "api@example.com",
+                password = "api_password",
+            )
+        self.TestStash = CardStash.objects.create(
+                name = 'ApiTestStash',
+            )
+
+    def test_no_action_returns_400( self ):
+        request = self.request_factory.get( reverse("api_index") )
+        response = index( request )
+        self.assertContains(response,'Malformed request',status_code=400)
+
+    def test_useless_action_returns_418( self ):
+        request = self.request_factory.get( reverse("api_index") )
+        request.GET = {
+                'action': 'Foo',
+            }
+        response = index( request )
+        self.assertContains(response,'',status_code=418)
+
+    def test_get_n_card_in( self ):
+        test_card = 'sv3-5'
+        test_quant = 4
+        self.TestStash.allocate(test_card,test_quant)
+        request = self.request_factory.get( 
+                reverse( 'api_get_n_card_in_stash', args=[self.TestStash.uuid] )
+            )
+        request.GET = {
+                'card_id': test_card,
+            }
+        response = get_n_card_in( request, self.TestStash.uuid )
+        self.assertContains( response, test_quant )
+
+    def test_bulk_update( self ):
--- cardsite/api/urls.py
+++ cardsite/api/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views, api
 
 urlpatterns = [
-        path( "", api.index),
-        path( "get_n_card_in/<uuid:stash_uuid>", api.get_n_card_in ),
-        path( "update/<uuid:stash_uuid>", api.update_stash ),
-        path( "bulkUpdate", api.move_cards_from_to ),
+        path( "", api.index, name = "api_index"),
+        path( "get_n_card_in/<uuid:stash_uuid>", api.get_n_card_in, name="api_get_n_card_in_stash" ),
+        path( "update/<uuid:stash_uuid>", api.update_stash, name="api_update_stash" ),
+        path( "bulkUpdate", api.move_cards_from_to, name="api_bulk_update" ),
     ]
 
